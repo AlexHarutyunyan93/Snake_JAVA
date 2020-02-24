020c3cd3e31f5b137daa8fdd7cf95c13c1c04a95
alex.harutyunyan <alex.harutyunyan@synisys.com>
2020-02-24 13:36:10 +0400

    initial commit

---
+++ src/com/codewithmosh/Display.java
@@ -0,0 +1,42 @@
+package com.codewithmosh;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Display extends JPanel implements ActionListener {
+    private final int B_WIDTH = 300;
+    private final int B_HEIGHT = 300;
+    private final int DOT_SIZE = 10;
+    private final int ALL_DOTS = 900;
+    private final int RAND_POS = 29;
+    private final int DELAY = 140;
+
+    private final int x[] = new int[ALL_DOTS];
+    private final int y[] = new int[ALL_DOTS];
+
+    private int dots;
+    private int apple_x;
+    private int apple_y;
+
+    private boolean leftDirection = false;
+    private boolean rightDirection = true;
+    private boolean upDirection = false;
+    private boolean downDirection = false;
+    private boolean inGame = true;
+
+    private Timer timer;
+    private Image body;
+    private Image apple;
+    private Image head;
+
+
+    private void initBoard() {
+
+        addKeyListener(new TAdapter());
+        setBackground(Color.black);
+        setFocusable(true);
+
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+    }
+}
---
+++ src/com/codewithmosh/KeyboardListener.java
@@ -0,0 +1,15 @@
+package com.codewithmosh;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+
+public class KeyboardListener extends KeyAdapter implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e, boolean inGame) {
+
+
+    }
+
+}
